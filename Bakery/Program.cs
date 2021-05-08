02419f8425278ebfd5bd602c5ceab70f115168e1
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-07 19:37:08 -0700

    add a display for the specials

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,18 +8,34 @@ namespace Bakery
     {
       Console.WriteLine("-----------------------------");
       Console.WriteLine("\n");
+      Console.WriteLine("\n");
       Console.WriteLine("---------( ͡° ͜ʖ ͡°( ͡° ͜ʖ ͡°) ͡° ͜ʖ ͡°)----------");
       Console.WriteLine("--)-@ Bienvenue à la boulangerie Pierre --)-@");
       Console.WriteLine("\n");
+      Console.WriteLine("\n");
       Console.WriteLine(" voir notre menu en français, entrer [\"Français\"]");
       Console.WriteLine(" To see our menu in English enter [\"English\"]");
-      Console.WriteLine("-----------------------------");
+      Console.WriteLine("\n");
+      Console.WriteLine("------------------------------------------");
       string answer = Console.ReadLine();
-      if(answer == "Enter" || answer == "english" || answer == "ENTER")
+      if(answer == "English" || answer == "english" || answer == "ENGLISH")
       {
-        Console.WriteLine(" Todays Specials ");
+        Console.WriteLine("\n");
+        Console.WriteLine("\n");
         Console.WriteLine("--)-@ Welcome to Pierre's Bakery --)-@");
-        Console.WriteLine(" Todays Specials Are");
+        Console.WriteLine("\n");
+        Console.WriteLine(" -     (☞ﾟヮﾟ)☞  Todays Specials  ☜(ﾟヮﾟ☜)   -");
+        Console.WriteLine(" -              ----------------            -");
+        Console.WriteLine(" -                  Baguettes:              -");
+        Console.WriteLine(" -           Buy 2 get 1 free for $10       -");
+        Console.WriteLine(" -                    Or                    -");
+        Console.WriteLine(" -             1 Baguette for $5            -");
+        Console.WriteLine(" -                                          -");
+        Console.WriteLine(" -                  Pastries:               -");
+        Console.WriteLine(" -           Buy 2 get 1 1/2 Off $5         -");
+        Console.WriteLine(" -                    Or                    -");
+        Console.WriteLine(" -             1 Pastry for $2              -");
+        Console.WriteLine("\n");
       }
       else
       {
