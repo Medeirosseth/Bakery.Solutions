d1c42bd4bc4e45613631c5677113134f18dec0a5
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-07 20:06:42 -0700

    WIP: troubleshooting namespace errors

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using Bakery.Models;
+using BakeryPastry;
 
 namespace Bakery
 {
@@ -6,42 +8,35 @@ namespace Bakery
   {
     public static void Main()
     {
-      Console.WriteLine("-----------------------------");
       Console.WriteLine("\n");
       Console.WriteLine("\n");
-      Console.WriteLine("---------( ͡° ͜ʖ ͡°( ͡° ͜ʖ ͡°) ͡° ͜ʖ ͡°)----------");
-      Console.WriteLine("--)-@ Bienvenue à la boulangerie Pierre --)-@");
+      Console.WriteLine(" - --'-(@ Welcome to Pierre's Bakery --,-(@ -");
+      Console.WriteLine(" -                                          -");
+      Console.WriteLine(" -     (☞ﾟヮﾟ)☞  Todays Specials  ☜(ﾟヮﾟ☜)   -");
+      Console.WriteLine(" -              ----------------            -");
+      Console.WriteLine(" -                  Baguettes:              -");
+      Console.WriteLine(" -           Buy 2 get 1 free for $10       -");
+      Console.WriteLine(" -                    Or                    -");
+      Console.WriteLine(" -             1 Baguette for $5            -");
+      Console.WriteLine(" -                                          -");
+      Console.WriteLine(" -                  Pastries:               -");
+      Console.WriteLine(" -           Buy 2 get 1 1/2 Off $5         -");
+      Console.WriteLine(" -                    Or                    -");
+      Console.WriteLine(" -             1 Pastry for $2              -");
       Console.WriteLine("\n");
+      Console.WriteLine("If you are here for Baguettes enter[\"B\"]");
+      Console.WriteLine("If you are here for Pastries enter[\"P\"]");
       Console.WriteLine("\n");
-      Console.WriteLine(" voir notre menu en français, entrer [\"Français\"]");
-      Console.WriteLine(" To see our menu in English enter [\"English\"]");
-      Console.WriteLine("\n");
-      Console.WriteLine("------------------------------------------");
-      string answer = Console.ReadLine();
-      if(answer == "English" || answer == "english" || answer == "ENGLISH")
+      string userAnswer = Console.ReadLine();
+      if(userAnswer == "B" || userAnswer == "b")
       {
-        Console.WriteLine("\n");
-        Console.WriteLine("\n");
-        Console.WriteLine("--)-@ Welcome to Pierre's Bakery --)-@");
-        Console.WriteLine("\n");
-        Console.WriteLine(" -     (☞ﾟヮﾟ)☞  Todays Specials  ☜(ﾟヮﾟ☜)   -");
-        Console.WriteLine(" -              ----------------            -");
-        Console.WriteLine(" -                  Baguettes:              -");
-        Console.WriteLine(" -           Buy 2 get 1 free for $10       -");
-        Console.WriteLine(" -                    Or                    -");
-        Console.WriteLine(" -             1 Baguette for $5            -");
-        Console.WriteLine(" -                                          -");
-        Console.WriteLine(" -                  Pastries:               -");
-        Console.WriteLine(" -           Buy 2 get 1 1/2 Off $5         -");
-        Console.WriteLine(" -                    Or                    -");
-        Console.WriteLine(" -             1 Pastry for $2              -");
-        Console.WriteLine("\n");
+        Console.WriteLine(" How many Baguettes will you be purchasing today");
+        Console.WriteLine(" Enter number below then press enter");
+        string userQuantity = Console.ReadLine();
+        CostOfBread();
       }
-      else
-      {
-        Console.WriteLine("try again");
 
-      }
+    
     }
   }
 }
