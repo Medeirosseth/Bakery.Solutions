873245759ba1d3b2f9640eb87e69f9b2afc10563
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-07 19:07:39 -0700

    add welcome message to UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -4,9 +4,29 @@ namespace Bakery
 {
   public class Program
   {
-    public static void main()
+    public static void Main()
     {
-      Console.WriteLine("Testing, Testing, 1, 2, 1, 2");
+      Console.WriteLine("-----------------------------");
+      Console.WriteLine("\n");
+      Console.WriteLine("---------( ͡° ͜ʖ ͡°( ͡° ͜ʖ ͡°) ͡° ͜ʖ ͡°)----------");
+      Console.WriteLine("--)-@ Bienvenue à la boulangerie Pierre --)-@");
+      Console.WriteLine("\n");
+      Console.WriteLine(" voir notre menu en français, entrer [\"Français\"]");
+      Console.WriteLine(" To see our menu in English enter [\"English\"]");
+      Console.WriteLine("-----------------------------");
+      string answer = Console.ReadLine();
+      if(answer == "Enter" || answer == "english" || answer == "ENTER")
+      {
+        Console.WriteLine(" Todays Specials ");
+        Console.WriteLine("--)-@ Welcome to Pierre's Bakery --)-@");
+        Console.WriteLine(" Todays Specials Are");
+      }
+      else
+      {
+        Console.WriteLine("try again");
+
+      }
     }
   }
 }
+
